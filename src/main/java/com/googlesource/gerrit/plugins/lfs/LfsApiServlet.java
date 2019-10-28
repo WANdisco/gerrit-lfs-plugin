292d35555c5752668b4bf77a7a51faf1adfe892b
Trevor Getty <trevor.getty@wandisco.com>
2019-10-28 23:12:58 +0000

    Applying WANdisco Replication Patch.
    WANdisco replication patch, to allow LFS support to replicated sites. Patch includes all changes up to 2.13.12_WDv3.

--- src/main/java/com/googlesource/gerrit/plugins/lfs/LfsApiServlet.java
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/LfsApiServlet.java
@@ -1,3 +1,16 @@
+
+/********************************************************************************
+ * Copyright (c) 2014-2018 WANdisco
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Apache License, Version 2.0
+ *
+ ********************************************************************************/
+ 
 // Copyright (C) 2016 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,10 +27,6 @@
 
 package com.googlesource.gerrit.plugins.lfs;
 
-import static com.google.gerrit.extensions.client.ProjectState.HIDDEN;
-import static com.google.gerrit.extensions.client.ProjectState.READ_ONLY;
-import static com.google.gerrit.httpd.plugins.LfsPluginServlet.URL_REGEX;
-
 import com.google.common.base.Strings;
 import com.google.gerrit.common.ProjectUtil;
 import com.google.gerrit.common.data.Capable;
@@ -28,19 +37,20 @@ import com.google.gerrit.server.project.ProjectControl;
 import com.google.gerrit.server.project.ProjectState;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-
-import org.eclipse.jgit.lfs.errors.LfsException;
-import org.eclipse.jgit.lfs.errors.LfsRepositoryNotFound;
-import org.eclipse.jgit.lfs.errors.LfsRepositoryReadOnly;
-import org.eclipse.jgit.lfs.errors.LfsUnauthorized;
-import org.eclipse.jgit.lfs.errors.LfsUnavailable;
-import org.eclipse.jgit.lfs.errors.LfsValidationError;
+import org.eclipse.jgit.lfs.errors.*;
 import org.eclipse.jgit.lfs.server.LargeFileRepository;
 import org.eclipse.jgit.lfs.server.LfsGerritProtocolServlet;
 import org.eclipse.jgit.lfs.server.LfsObject;
+import org.eclipse.jgit.lfs.server.ReplicationInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static com.google.gerrit.extensions.client.ProjectState.HIDDEN;
+import static com.google.gerrit.extensions.client.ProjectState.READ_ONLY;
+import static com.google.gerrit.httpd.LfsProjectParser.parseProjectFromPath;
+import static com.google.gerrit.httpd.plugins.LfsPluginServlet.URL_REGEX;
 
 @Singleton
 public class LfsApiServlet extends LfsGerritProtocolServlet {
@@ -53,6 +63,7 @@ public class LfsApiServlet extends LfsGerritProtocolServlet {
   private final LfsConfigurationFactory lfsConfigFactory;
   private final LfsRepositoryResolver repoResolver;
   private final LfsAuthUserProvider userProvider;
+  private static final Logger logger = LoggerFactory.getLogger(LfsApiServlet.class);
 
   @Inject
   LfsApiServlet(ProjectCache projectCache,
@@ -69,28 +80,29 @@ public class LfsApiServlet extends LfsGerritProtocolServlet {
   protected LargeFileRepository getLargeFileRepository(
       LfsRequest request, String path, String auth)
           throws LfsException {
-    String pathInfo = path.startsWith("/") ? path : "/" + path;
-    Matcher matcher = URL_PATTERN.matcher(pathInfo);
-    if (!matcher.matches()) {
-      throw new LfsException("no repository at " + pathInfo);
+
+    String projName = null;
+    Project.NameKey project = null;
+    try {
+      projName = parseProjectFromPath(path);
+      project = Project.NameKey.parse(ProjectUtil.stripGitSuffix(projName));
+
+    } catch (Exception e) {
+      logger.error("Repository requested wasn't found for path: " + path);
+      throw new LfsRepositoryNotFound(projName == null ? path : projName);
     }
-    String projName = matcher.group(1);
-    Project.NameKey project = Project.NameKey.parse(
-        ProjectUtil.stripGitSuffix(projName));
+
     ProjectState state = projectCache.get(project);
     if (state == null || state.getProject().getState() == HIDDEN) {
       throw new LfsRepositoryNotFound(project.get());
     }
-    authorizeUser(userProvider.getUser(auth, projName, request.getOperation()),
-        state, request.getOperation());
+    authorizeUser(userProvider.getUser(auth, projName, request.getOperation()), state, request.getOperation());
 
-    if (request.getOperation().equals(UPLOAD)
-        && state.getProject().getState() == READ_ONLY) {
+    if (request.getOperation().equals(UPLOAD) && state.getProject().getState() == READ_ONLY) {
       throw new LfsRepositoryReadOnly(project.get());
     }
 
-    LfsProjectConfigSection config =
-        lfsConfigFactory.getProjectsConfig().getForProject(project);
+    LfsProjectConfigSection config = lfsConfigFactory.getProjectsConfig().getForProject(project);
     // Only accept requests for projects where LFS is enabled.
     // No config means we default to "not enabled".
     if (config != null && config.isEnabled()) {
@@ -104,15 +116,26 @@ public class LfsApiServlet extends LfsGerritProtocolServlet {
         if (maxObjectSize > 0) {
           for (LfsObject object : request.getObjects()) {
             if (object.getSize() > maxObjectSize) {
-              throw new LfsValidationError(String.format(
-                  "size of object %s (%d bytes) exceeds limit (%d bytes)",
-                  object.getOid(), object.getSize(), maxObjectSize));
+              throw new LfsValidationError(
+                  String.format("size of object %s (%d bytes) exceeds limit (%d bytes)", object.getOid(), object.getSize(), maxObjectSize));
             }
           }
         }
       }
 
-      return repoResolver.get(project, config.getBackend());
+      LargeFileRepository repo = repoResolver.get(project, config.getBackend());
+      ReplicationInfo replicationInfo = new ReplicationInfo();
+      replicationInfo.isReplica = true;
+      replicationInfo.repositoryName = projName;
+
+      // TODO What about the repoId should it be here at all?, how to we get this in gerrit? We can use the name
+      // and let gitMS return more information to us when we eventually request info about this object?
+      // I will do that for now, and when it returns the ID we can cache it off for later re-use.
+      // replicationInfo.repositoryId =
+
+      // TODO Test project name  in subdirectories, as we need it to be unique!
+      repo.setReplicationInfo( replicationInfo );
+      return repo;
     }
 
     throw new LfsUnavailable(project.get());
