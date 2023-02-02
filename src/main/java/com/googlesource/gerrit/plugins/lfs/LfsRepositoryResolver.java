555b0f639e1ef4aa4f1e3cafcd92946af509c85f
Adam Johnston <adam.johnston@wandisco.com>
2023-02-02 12:21:20 +0000

    Gerrit with WANdisco Replication - v2.13.12-RP-1.9.6.2 =====================
    New Features
    ------------
    
    - Adding WANdisco replication support.
    
    - Added reno release notes with config.
    
     Squashed commits from: a36056a5c5d371aa778ac91b7de39bc869217031 to 210fd353b17085c2659d8d75304b9e8a9ad0fb11

--- src/main/java/com/googlesource/gerrit/plugins/lfs/LfsRepositoryResolver.java
+++ src/main/java/com/googlesource/gerrit/plugins/lfs/LfsRepositoryResolver.java
@@ -1,3 +1,16 @@
+
+/********************************************************************************
+ * Copyright (c) 2014-2018 WANdisco
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Apache License, Version 2.0
+ *
+ ********************************************************************************/
+ 
 // Copyright (C) 2016 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,11 +33,14 @@ import com.google.common.base.Strings;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.inject.Inject;
 
+import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lfs.errors.LfsRepositoryNotFound;
 import org.eclipse.jgit.lfs.server.LargeFileRepository;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class LfsRepositoryResolver {
@@ -34,11 +50,13 @@ public class LfsRepositoryResolver {
   private final LfsRepositoriesCache cache;
   private final LfsBackend defaultBackend;
   private final Map<String, LfsBackend> backends;
+  private final LfsConfigurationFactory configFactory;
 
   @Inject
   LfsRepositoryResolver(LfsRepositoriesCache cache,
       LfsConfigurationFactory configFactory) {
     this.cache = cache;
+    this.configFactory = configFactory;
 
     LfsGlobalConfig config = configFactory.getGlobalConfig();
     this.defaultBackend = config.getDefaultBackend();
@@ -47,18 +65,7 @@ public class LfsRepositoryResolver {
 
   public LargeFileRepository get(Project.NameKey project, String backendName)
       throws LfsRepositoryNotFound {
-    LfsBackend backend;
-    if (Strings.isNullOrEmpty(backendName)) {
-      backend = defaultBackend;
-    } else {
-      backend = backends.get(backendName);
-      if (backend == null) {
-        log.error(String.format("Project %s is configured with not existing"
-            + " backend %s", project,
-            Strings.isNullOrEmpty(backendName) ? DEFAULT : backendName));
-        throw new LfsRepositoryNotFound(project.get());
-      }
-    }
+    final LfsBackend backend = getLfsBackend(project, backendName);
 
     LargeFileRepository repository = cache.get(backend);
     if (repository != null) {
@@ -70,6 +77,62 @@ public class LfsRepositoryResolver {
         + " backend %s of type %s", project,
         Strings.isNullOrEmpty(backendName) ? DEFAULT : backendName,
         backend.type));
+    throw new LfsRepositoryNotFound(project.get());
+  }
+
+  /**
+   * Returns the LfsBackend which has been configured for a particular repository
+   *
+   * Defaults allowReload=false to keep previous behaviour.
+   *
+   * @param project
+   * @param backendName
+   * @return
+   * @throws LfsRepositoryNotFound
+   */
+  private LfsBackend getLfsBackend(Project.NameKey project, String backendName) throws LfsRepositoryNotFound {
+    return getLfsBackend(project, backendName, false);
+  }
+
+    /**
+     * Returns the LfsBackend which has been configured for a particular repository
+     *
+     * Additional reload=true behaviour allows the backend cache to be reloaded, if it fails to find an entry, possible because
+     * the lfs plugin configuration has been changed online.
+     * @param project
+     * @param backendName
+     * @return
+     * @throws LfsRepositoryNotFound
+     */
+  private synchronized LfsBackend getLfsBackend(Project.NameKey project, String backendName, boolean allowReload) throws LfsRepositoryNotFound {
+    LfsBackend backend;
+    if (Strings.isNullOrEmpty(backendName)) {
+      backend = defaultBackend;
+      return backend;
+    }
+
+    backend = backends.get(backendName);
+    if (backend != null) {
+      return backend;
+    }
+
+    // before we throw, decide if a reload has been allowed.
+    if (allowReload) {
+      configFactory.forceReloadOfGlobalConfig(configFactory.getPluginName());
+
+      // reload the backends, by adding any missing items to the backend list.
+      LfsGlobalConfig config = configFactory.getGlobalConfig();
+      this.backends.putAll(config.getBackends());
+
+      // if it has reload the cache here and call ourself with FALSE for reload, so it will throw next time,
+      // or succeed if the cache has been updated.
+      return getLfsBackend(project, backendName, false);
+    }
+
+    // we didn't find the backend, throw exception.
+    log.error(String.format("Project %s is configured with not existing"
+            + " backend %s", project,
+        Strings.isNullOrEmpty(backendName) ? DEFAULT : backendName));
     throw new LfsRepositoryNotFound(project.get());
   }
 }
